5d5dece83459186dfd15e5e5cc3b3fc902619372
Giovana <giirochas@hotmail.com>
2022-12-27 13:09:12 -0300

    Init frontend app

---
+++ desafio-programacao-fullstack-1.1.0/frontend/src/App.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+
+class App extends Component {
+  render () {
+    <div className="App">
+     
+    </div>
+  }
+}
+
+export default App;
